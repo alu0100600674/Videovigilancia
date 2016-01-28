bd4b06a4fa3eee62e1231365b7e8095e6ad8cade
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2016-01-28 17:08:48 +0000

    Primeros pasos

--- app/src/main/java/com/tfg/jonay/videovigilancia/MainActivityFragment.java
+++ app/src/main/java/com/tfg/jonay/videovigilancia/MainActivityFragment.java
@@ -1,10 +1,12 @@
 package com.tfg.jonay.videovigilancia;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -17,6 +19,17 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View v_principal = inflater.inflate(R.layout.fragment_main, container, false);
+
+        Button btn_camera = (Button) getView().findViewById(R.id.btn_camera);
+        btn_camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
+
+        return v_principal;
     }
 }
