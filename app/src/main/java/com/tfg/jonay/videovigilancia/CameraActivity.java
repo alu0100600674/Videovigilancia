4d54ef024efdd14d7fa9ef98777f674ea6c94caf
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2016-01-28 18:08:32 +0000

    Encendiendo el flash 3 segundos

--- app/src/main/java/com/tfg/jonay/videovigilancia/CameraActivity.java
+++ app/src/main/java/com/tfg/jonay/videovigilancia/CameraActivity.java
@@ -1,7 +1,10 @@
 package com.tfg.jonay.videovigilancia;
 
+import android.hardware.Camera;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import java.security.Policy;
 
 public class CameraActivity extends AppCompatActivity {
 
@@ -9,5 +12,18 @@ public class CameraActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
+
+        Camera cam = Camera.open();
+        Camera.Parameters p = cam.getParameters();
+        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
+        cam.setParameters(p);
+        cam.startPreview();
+
+        long start = System.currentTimeMillis();
+        long end = start + 3*1000;
+        while (System.currentTimeMillis() < end){}
+
+        cam.stopPreview();
+        cam.release();
     }
 }
