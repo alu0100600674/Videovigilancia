e54009b2a88c144ae60f3f717116f342ea429ed7
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2016-01-29 18:56:02 +0000

    Mostrando preview de la cámara. Relación aspecto mal

--- app/src/main/java/com/tfg/jonay/videovigilancia/CameraActivity.java
+++ app/src/main/java/com/tfg/jonay/videovigilancia/CameraActivity.java
@@ -1,29 +1,65 @@
 package com.tfg.jonay.videovigilancia;
 
+import android.content.Context;
 import android.hardware.Camera;
+import android.hardware.camera2.CameraAccessException;
+import android.hardware.camera2.CameraDevice;
+import android.hardware.camera2.CameraManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Surface;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
+import java.io.IOException;
+import java.lang.reflect.Array;
 import java.security.Policy;
 
 public class CameraActivity extends AppCompatActivity {
+
+    private Button boton;
+    private SurfaceView surfaceView;
+    private SurfaceHolder surfaceHolder;
+    private Camera camara;
+    private boolean encendida;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_camera);
 
-        Camera cam = Camera.open();
-        Camera.Parameters p = cam.getParameters();
-        p.setFlashMode(Camera.Parameters.FLASH_MODE_TORCH);
-        cam.setParameters(p);
-        cam.startPreview();
+        boton = (Button) findViewById(R.id.ver_video);
+        surfaceView = (SurfaceView) findViewById(R.id.view_cam);
+        surfaceHolder = surfaceView.getHolder();
+        encendida = false;
 
-        long start = System.currentTimeMillis();
-        long end = start + 3*1000;
-        while (System.currentTimeMillis() < end){}
+        boton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try {
+                    startCamara();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
 
-        cam.stopPreview();
-        cam.release();
+    private void startCamara() throws IOException{
+        if(!encendida){
+            camara = Camera.open();
+            camara.setPreviewDisplay(surfaceHolder);
+            camara.startPreview();
+            encendida = true;
+        }else{
+            camara.stopPreview();
+            camara.release();
+            encendida = false;
+        }
     }
 }
