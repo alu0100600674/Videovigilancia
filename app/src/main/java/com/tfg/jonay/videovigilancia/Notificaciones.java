de9e186734ef4e1224bad4702edce9392c871634
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2016-02-11 18:27:17 +0000

    Añadidos métodos a la clase Notificaciones para añadir y eliminar destinatarios y enviar sms movimiento

--- app/src/main/java/com/tfg/jonay/videovigilancia/Notificaciones.java
+++ app/src/main/java/com/tfg/jonay/videovigilancia/Notificaciones.java
@@ -1,7 +1,10 @@
 package com.tfg.jonay.videovigilancia;
 
+import android.content.res.Resources;
 import android.telephony.SmsManager;
 import android.text.format.Time;
+
+import java.util.ArrayList;
 
 /**
  * Created by jonay on 11/02/16.
@@ -9,20 +12,39 @@ import android.text.format.Time;
 public class Notificaciones {
 
     private String numero = "628435740";
-    private String numero2 = "636949909";
     private SmsManager sms_manager;
+    private ArrayList<String> destinatarios;
+    private String nombre_app;
 
-    public Notificaciones(){
+    public Notificaciones(String app_name){
+        nombre_app = app_name;
         sms_manager = SmsManager.getDefault();
-//        sms_manager.sendTextMessage(numero, null, mensaje_movimiento, null, null);
-//        System.out.println("Mensaje enviado... a " + numero);
+        destinatarios = new ArrayList<>();
+    }
+
+    public void addDestinatario(String numero){
+        destinatarios.add(numero);
+        String mensaje = nombre_app + ": Ha sido añadido a la lista de notificaciones.";
+        sms_manager.sendTextMessage(numero, null, mensaje, null, null);
+    }
+
+    public void delDestinatario(String numero){
+        for(int i = 0; i < destinatarios.size(); i++){
+            if(destinatarios.get(i) == numero){
+                String mensaje = nombre_app + ": Ha sido eliminado de la lista de notificaciones.";
+                sms_manager.sendTextMessage(numero, null, mensaje, null, null);
+                destinatarios.remove(i);
+            }
+        }
     }
 
     public void enviarSmsMovimiento(){
         Time now = new Time(Time.getCurrentTimezone());
         now.setToNow();
-        String mensaje = "Movimiento detectado el día " + now.monthDay + "/" + now.month + "/" + now.year + " a las " + now.format("%k:%M:%S") + ".";
-        sms_manager.sendTextMessage(numero, null, mensaje, null, null);
+        String mensaje = nombre_app + ": Movimiento detectado el día " + now.monthDay + "/" + now.month + "/" + now.year + " a las " + now.format("%k:%M:%S") + ".";
+        for(int i = 0; i < destinatarios.size(); i++){
+            sms_manager.sendTextMessage(destinatarios.get(i), null, mensaje, null, null);
+        }
     }
 
 }
