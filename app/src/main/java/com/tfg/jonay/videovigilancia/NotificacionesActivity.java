7a844f7fec09063d9f11a2d59b8047a2eb0dc140
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2016-02-13 19:45:48 +0000

    Botón para añadir destino en notificaciones casi OK

--- app/src/main/java/com/tfg/jonay/videovigilancia/NotificacionesActivity.java
+++ app/src/main/java/com/tfg/jonay/videovigilancia/NotificacionesActivity.java
@@ -1,7 +1,14 @@
 package com.tfg.jonay.videovigilancia;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.net.Uri;
+import android.provider.ContactsContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class NotificacionesActivity extends AppCompatActivity {
 
@@ -9,5 +16,46 @@ public class NotificacionesActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notificaciones);
+
+        Button btn_add_destino = (Button) findViewById(R.id.btn_add_destinatario);
+        btn_add_destino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent_contactos = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);;
+                startActivityForResult(intent_contactos, 1);
+            }
+        });
+
+        Button btn_del_destino = (Button) findViewById(R.id.btn_del_destinatario);
+        btn_del_destino.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        switch(requestCode){
+            case(1):
+                if(resultCode == Activity.RESULT_OK){
+                    Uri contactData = data.getData();
+                    Cursor c = getContentResolver().query(contactData, null, null, null, null);
+                    String numero = null;
+                    String id = null;
+                    if(c.moveToFirst()){
+                        id = c.getString(c.getColumnIndex(ContactsContract.Contacts._ID));
+                        numero = c.getString(c.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME));
+                    }
+                    if(numero != null){
+                        // Aquí va el método que añade el destinatario.
+                    }
+                }
+                break;
+        }
+    }
+
 }
