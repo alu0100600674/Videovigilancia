549f6cb3c4032395e45d76ec593c5904c5dbe1b3
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2016-04-24 00:17:48 +0100

    Añadida librería para la detección de movimiento

---
+++ app/src/main/java/com/tfg/jonay/videovigilancia/DeteccionDeMovimiento.java
@@ -0,0 +1,21 @@
+package com.tfg.jonay.videovigilancia;
+
+/**
+ * Created by jonay on 11/04/16.
+ */
+public class DeteccionDeMovimiento {
+
+    private Thread threadMov;
+
+    public DeteccionDeMovimiento(){
+
+    }
+
+    public void iniciarDeteccion(){
+
+    }
+
+    public void pararDeteccion(){
+
+    }
+}
