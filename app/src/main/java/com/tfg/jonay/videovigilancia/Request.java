db612c36d4acfa4a81f5e54299334c797acb4496
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2016-06-02 16:45:37 +0100

    Enviando los request cifrados

--- app/src/main/java/com/tfg/jonay/videovigilancia/Request.java
+++ app/src/main/java/com/tfg/jonay/videovigilancia/Request.java
@@ -13,25 +13,53 @@ import com.android.volley.toolbox.JsonObjectRequest;
  * Created by jonay on 9/04/16.
  */
 public class Request {
+
+    /* new User con datos sin cifrar */
+//    //Función que registra a un usuario en el servicio web la primera vez que usa la app
+//    public static void newUser(String MAC, RequestQueue requestQueue, String stream_short_url, String web_url, String ip_actual) {
+//
+//        HashMap<String, String> params = new HashMap<String, String>();
+//        params.put("name", MAC);
+//        params.put("server", stream_short_url);
+//        params.put("ipcamara", ip_actual);
+//
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(com.android.volley.Request.Method.POST, web_url + "/camara", new JSONObject(params),
+//                new com.android.volley.Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        Log.d("Volley nuevoUsuario Request ", response.toString());
+//                    }
+//                },
+//                new com.android.volley.Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.d("Volley nuevoUsuario Request Error ", error.toString());
+//                    }
+//                });
+//
+//        requestQueue.add(jsonObjectRequest);
+//    }
+
+    /* new User con datos cifrados */
     //Función que registra a un usuario en el servicio web la primera vez que usa la app
     public static void newUser(String MAC, RequestQueue requestQueue, String stream_short_url, String web_url, String ip_actual) {
 
         HashMap<String, String> params = new HashMap<String, String>();
-        params.put("name", MAC);
-        params.put("server", stream_short_url);
-        params.put("ipcamara", ip_actual);
+        params.put("name", AES.cifrar(MAC));
+        params.put("server", AES.cifrar(stream_short_url));
+        params.put("ipcamara", AES.cifrar(ip_actual));
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(com.android.volley.Request.Method.POST, web_url + "/camara", new JSONObject(params),
                 new com.android.volley.Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.d("Volley nuevoUsuario Request ", response.toString());
+                        Log.d("nUsuario Request ", response.toString());
                     }
                 },
                 new com.android.volley.Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.d("Volley nuevoUsuario Request Error ", error.toString());
+                        Log.d("nUsuario Request Error ", error.toString());
                     }
                 });
 
@@ -60,24 +88,50 @@ public class Request {
 //        requestQueue.add(jsonObjectRequest);
 //    }
 
+    /* streamOnline con datos sin cifrar */
+    //Función que establece un video como online
+//    public static void streamOnline(String MAC, RequestQueue requestQueue, String web_url, String ip_actual, String stream_short_url) {
+//        HashMap<String, String> params = new HashMap<String, String>();
+//        params.put("name", MAC);
+//        params.put("ipcamara", ip_actual);
+//        params.put("server", stream_short_url);
+//
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(com.android.volley.Request.Method.PUT, web_url + "/online/" + MAC, new JSONObject(params),
+//                new com.android.volley.Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        Log.d("Volley streamOnline Request ", response.toString());
+//                    }
+//                },
+//                new com.android.volley.Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.d("Volley streamOnline Request Error ", error.toString());
+//                    }
+//                });
+//
+//        requestQueue.add(jsonObjectRequest);
+//    }
+
+    /* streamOnline con datos cifrados */
     //Función que establece un video como online
     public static void streamOnline(String MAC, RequestQueue requestQueue, String web_url, String ip_actual, String stream_short_url) {
         HashMap<String, String> params = new HashMap<String, String>();
-        params.put("name", MAC);
-        params.put("ipcamara", ip_actual);
-        params.put("server", stream_short_url);
+        params.put("name", AES.cifrar(MAC));
+        params.put("ipcamara", AES.cifrar(ip_actual));
+        params.put("server", AES.cifrar(stream_short_url));
 
         JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(com.android.volley.Request.Method.PUT, web_url + "/online/" + MAC, new JSONObject(params),
                 new com.android.volley.Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.d("Volley streamOnline Request ", response.toString());
+                        Log.d("Online Request ", response.toString());
                     }
                 },
                 new com.android.volley.Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.d("Volley streamOnline Request Error ", error.toString());
+                        Log.d("Online Request Error ", error.toString());
                     }
                 });
 
@@ -85,7 +139,28 @@ public class Request {
     }
 
 
+    /* streamOffline con datos sin cifrar */
+//    //Función que establece un video como offline
+//    public static void streamOffline(String MAC, RequestQueue requestQueue, String web_url) {
+//
+//        JsonObjectRequest jsonObjectRequest = new JsonObjectRequest(com.android.volley.Request.Method.PUT, web_url + "/offline/" + MAC,
+//                new com.android.volley.Response.Listener<JSONObject>() {
+//                    @Override
+//                    public void onResponse(JSONObject response) {
+//                        Log.d("Volley streamOffline Request ", response.toString());
+//                    }
+//                },
+//                new com.android.volley.Response.ErrorListener() {
+//                    @Override
+//                    public void onErrorResponse(VolleyError error) {
+//                        Log.d("Volley streamOffline Request Error ", error.toString());
+//                    }
+//                });
+//
+//        requestQueue.add(jsonObjectRequest);
+//    }
 
+    /* streamOffline con datos cifrados */
     //Función que establece un video como offline
     public static void streamOffline(String MAC, RequestQueue requestQueue, String web_url) {
 
@@ -93,13 +168,13 @@ public class Request {
                 new com.android.volley.Response.Listener<JSONObject>() {
                     @Override
                     public void onResponse(JSONObject response) {
-                        Log.d("Volley streamOffline Request ", response.toString());
+                        Log.d("Offline Request ", response.toString());
                     }
                 },
                 new com.android.volley.Response.ErrorListener() {
                     @Override
                     public void onErrorResponse(VolleyError error) {
-                        Log.d("Volley streamOffline Request Error ", error.toString());
+                        Log.d("Offline Request Error ", error.toString());
                     }
                 });
 
