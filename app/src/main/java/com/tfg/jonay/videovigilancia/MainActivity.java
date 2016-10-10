35b7fb9884ecca26ff6a9d0ca03f8601bfa47036
Jonay Suárez Armas <alu0100600674@ull.edu.es>
2016-10-10 12:37:32 +0100

    Cambio de forma de obtener la MAC para Android 6

--- app/src/main/java/com/tfg/jonay/videovigilancia/MainActivity.java
+++ app/src/main/java/com/tfg/jonay/videovigilancia/MainActivity.java
@@ -23,6 +23,7 @@ import com.charlie.ev3.OutputPort;
 
 import java.io.IOException;
 import java.io.UnsupportedEncodingException;
+import java.net.NetworkInterface;
 import java.security.InvalidAlgorithmParameterException;
 import java.security.InvalidKeyException;
 import java.security.KeyFactory;
@@ -38,6 +39,8 @@ import java.security.spec.ECGenParameterSpec;
 import java.security.spec.InvalidKeySpecException;
 import java.security.spec.PKCS8EncodedKeySpec;
 import java.security.spec.X509EncodedKeySpec;
+import java.util.Collections;
+import java.util.List;
 
 import javax.crypto.KeyAgreement;
 
@@ -70,9 +73,11 @@ public class MainActivity extends AppCompatActivity {
         globales.ini_serv();
         globales.getServidor().cargarDesdeBDD(globales.getBaseDeDatos().getServData());
         globales.getServidor().inicializarRequestQueue(this);
+
         WifiManager wifiManager = (WifiManager) getSystemService(Context.WIFI_SERVICE);
-        WifiInfo wInfo = wifiManager.getConnectionInfo();
-        globales.getServidor().setMacAddress(wInfo.getMacAddress());
+//        WifiInfo wInfo = wifiManager.getConnectionInfo();
+//        globales.getServidor().setMacAddress(wInfo.getMacAddress());
+        globales.getServidor().setMacAddress(getMacAddr());
         globales.getServidor().setIpActual(Formatter.formatIpAddress(wifiManager.getConnectionInfo().getIpAddress()));
 
         globales.ini_robot();
@@ -125,5 +130,31 @@ public class MainActivity extends AppCompatActivity {
 //        }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    private String getMacAddr() {
+        try {
+            List<NetworkInterface> all = Collections.list(NetworkInterface.getNetworkInterfaces());
+            for (NetworkInterface nif : all) {
+                if (!nif.getName().equalsIgnoreCase("wlan0")) continue;
+
+                byte[] macBytes = nif.getHardwareAddress();
+                if (macBytes == null) {
+                    return "";
+                }
+
+                StringBuilder res1 = new StringBuilder();
+                for (byte b : macBytes) {
+                    res1.append(String.format("%02X:",b));
+                }
+
+                if (res1.length() > 0) {
+                    res1.deleteCharAt(res1.length() - 1);
+                }
+                return res1.toString();
+            }
+        } catch (Exception ex) {
+        }
+        return "02:00:00:00:00:00";
     }
 }
